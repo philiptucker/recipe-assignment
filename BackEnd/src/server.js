05c65c537e3ac4291393e92f441e5ec83010baa3
philiptucker <philip.tucker37@ed.cna.nl.ca>
2023-10-02 15:39:43 -0400

    added bodyparser to backend

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -1,9 +1,20 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
+// cd recipe-assignment/backend
+// npm run dev
 
 const app = express();
+app.use(express.json());
 
-app.get('/hello', (req, res) =>{
-    res.send('Hello!');
+app.post('/hello', (req, res) =>{
+    console.log(req.body);
+    res.send(`Hello ${req.body.name} !`);
+});
+
+app.get('/hello/:name', (req, res) => {
+    const {name} = req.params;
+    res.send(`Hello ${name}!?!`);
 });
 
 app.listen(8000, () => {
